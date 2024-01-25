dc84f51ed8c165387b9a21d1e816a548d93a970b
Diego Duarte <duartqx@gmail.com>
2024-01-25 00:14:07 -0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ddgodeliv
+
+go 1.21.6
---
+++ src/presentation/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hello ddgodeliv</h1>
+    </>
+  );
+}
+
+export default App;
