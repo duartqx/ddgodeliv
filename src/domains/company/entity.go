4013f214673e19227096a8daf67719c198337c15
Diego Duarte <duartqx@gmail.com>
2024-01-25 21:28:12 -0300

    domains company

---
+++ src/domains/company/entity.go
@@ -0,0 +1,32 @@
+package company
+
+type ICompany interface {
+	GetId() int
+	SetId(id int) ICompany
+
+	GetName() string
+	SetName(name string) ICompany
+}
+
+type Company struct {
+	Id   int    `db:"id" json:"id"`
+	Name string `db:"name" json:"name"`
+}
+
+func (c Company) GetId() int {
+	return c.Id
+}
+
+func (c *Company) SetId(id int) ICompany {
+	c.Id = id
+	return c
+}
+
+func (c Company) GetName() string {
+	return c.Name
+}
+
+func (c *Company) SetName(name string) ICompany {
+	c.Name = name
+	return c
+}
