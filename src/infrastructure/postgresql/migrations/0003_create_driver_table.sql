f3061843084a6cddc61617833c25ec16433c86c7
Diego Duarte <duartqx@gmail.com>
2024-01-26 23:26:50 -0300

    tweaks migrations

--- src/infrastructure/postgresql/migrations/0003_create_driver_table.sql
+++ src/infrastructure/postgresql/migrations/0003_create_driver_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS Drivers (
     id serial PRIMARY KEY,
-    user_id int REFERENCES users(id),
-    company_id int REFERENCES companies(id),
-    license_id VARCHAR(255) NOT NULL UNIQUE
+    user_id int NOT NULL,
+    company_id int NOT NULL,
+    license_id VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES Companies (id) ON DELETE CASCADE
 );
--- src/infrastructure/postgresql/migrations/0005_create_vehicle_table.sql
+++ src/infrastructure/postgresql/migrations/0005_create_vehicle_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS Vehicles (
     id SERIAL PRIMARY KEY,
-    model_id int NOT NULL REFERENCES VehicleModels(id),
-    company_id int NOT NULL REFERENCES Companies(id),
-    license_id VARCHAR(255) NOT NULL UNIQUE
+    model_id int NOT NULL,
+    company_id int NOT NULL,
+    license_id VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (model_id) REFERENCES VehicleModels (id) ON DELETE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES Companies (id) ON DELETE CASCADE
 );
