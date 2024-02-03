c0e1223efa05e3b44627efeabdce853354e258c1
Diego Duarte <duartqx@gmail.com>
2024-02-03 20:17:44 -0300

    errors ValidationError

--- src/application/errors/errors.go
+++ src/application/errors/errors.go
@@ -1,8 +1,28 @@
 package errors
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+)
 
 var (
-	BadRequestError = fmt.Errorf("Bad Request")
-	InternalError   = fmt.Errorf("Internal")
+	BadRequestError = errors.New("Bad Request")
+	InternalError   = errors.New("Internal")
+	ForbiddenError  = errors.New("Forbidden")
 )
+
+type ValidationError struct {
+	Message string
+}
+
+func (e *ValidationError) Error() string {
+	return e.Message
+}
+
+func (e ValidationError) Decode() *map[string]interface{} {
+	var res map[string]interface{}
+	if err := json.Unmarshal([]byte(e.Message), &res); err != nil {
+		return nil
+	}
+	return &res
+}
