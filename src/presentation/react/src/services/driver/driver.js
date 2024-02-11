22f4f43fea2804f0c5c6dc002bf17562398d2fdb
Diego Duarte <duartqx@gmail.com>
2024-02-11 20:01:12 -0300

    driverService get cache

--- src/presentation/react/src/services/driver/driver.js
+++ src/presentation/react/src/services/driver/driver.js
@@ -20,7 +20,30 @@ import httpClient from "../client";
 /** @returns {Promise<Driver[]>} */
 async function companyDrivers() {
   try {
+    const authData = JSON.parse(localStorage.getItem("auth") || "{}");
+    const cacheDriverKey = `cachedDriver__${authData?.token}`;
+
+    const cachedDriver = JSON.parse(
+      localStorage.getItem(cacheDriverKey) || "{}"
+    );
+
+    if (
+      cachedDriver?.expiresAt &&
+      (new Date(cachedDriver?.expiresAt) > new Date())
+    ) {
+      return cachedDriver.drivers;
+    }
+
     const res = await httpClient().get("/driver");
+
+    if (res.data) {
+      const newCachedDrivers = {
+        drivers: res.data,
+        expiresAt: new Date().setMinutes(new Date().getMinutes() + 5),
+      };
+      localStorage.setItem(cacheDriverKey, JSON.stringify(newCachedDrivers));
+    }
+
     return res.data || [];
   } catch (e) {
     console.log(e);
