471f60b3bd8b6ba3b508d3fc2ee3e6bdf1c0d125
Diego Duarte <duartqx@gmail.com>
2024-02-11 19:59:56 -0300

    sidebar listings update

--- src/presentation/react/src/components/SideBarListings.jsx
+++ src/presentation/react/src/components/SideBarListings.jsx
@@ -4,7 +4,7 @@ export default function SideBarListings({ listing, createButton }) {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 border-right bg-body-tertiary shadow"
-      style={{ width: "19rem", maxHeight: "100vh", position: "relative" }}
+      style={{ maxHeight: "100vh", height: "100vh" }}
     >
       <div style={{ overflowY: "auto", paddingBottom: "6rem" }}>{listing}</div>
       {createButton}
