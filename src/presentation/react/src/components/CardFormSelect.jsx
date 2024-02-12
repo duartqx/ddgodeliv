5c3ecc9a8e197e25495362e8c9c83136dc47e595
Diego Duarte <duartqx@gmail.com>
2024-02-12 17:02:26 -0300

    CardFormSelect

---
+++ src/presentation/react/src/components/CardFormSelect.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+/**
+ * @typedef {{
+ *  label: string
+ *  onChangeHandler: Function
+ *  options: { value: string | number, label: string}
+ *  required: boolean
+ * }} CardFormSelectObject
+ */
+
+/** @param {CardFormSelectObject} props */
+export default function CardFormSelect({
+  label,
+  onChangeHandler,
+  options,
+  required = true,
+}) {
+  return (
+    <div className="form-group mb-3">
+      <label className="text-body-tertiary fw-light">{label}</label>
+      <select onChange={onChangeHandler} required={required} className="form-select">
+        <option value="">Select One</option>
+        {options.map((o) => (
+          <option
+            value={o.value}
+            key={`select__option__${o.value}__${o.label.replace(" ", "")}`}
+          >
+            {o.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
