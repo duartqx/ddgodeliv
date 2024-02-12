f11ed52743647558dab2f050d927415ab10c2ec1
Diego Duarte <duartqx@gmail.com>
2024-02-12 17:03:45 -0300

    added option to use CardFormSelect and error

--- src/presentation/react/src/components/CardForm.jsx
+++ src/presentation/react/src/components/CardForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CardFormInput from "./CardFormInput";
-import Error from "./Error"
+import CardFormSelect from "./CardFormSelect";
+import Error from "./Error";
 
 /** @param {{
  *  title: string
@@ -17,7 +18,7 @@ export default function CardForm({ title, error, handleSubmit, inputs }) {
         position: "absolute",
         zIndex: 2,
         bottom: "5.6rem",
-        left: "5rem",
+        left: "0.5rem",
         width: "17rem",
       }}
     >
@@ -28,10 +29,26 @@ export default function CardForm({ title, error, handleSubmit, inputs }) {
         {title}
       </div>
       <div className="card-body shadow">
-        <div className="py-4">{error && <Error err={error} noPadding={true} />}</div>
+        <div className="py-4">
+          {error && <Error err={error} noPadding={true} />}
+        </div>
         <form action="post" onSubmit={handleSubmit}>
-
-          {inputs.map((props) => <CardFormInput {...props} />)}
+          {inputs.map((props) => {
+            if (props.type === "select") {
+              return (
+                <CardFormSelect
+                  {...props}
+                  key={`${props.label.replace(" ", "")}__${props.type}`}
+                />
+              );
+            }
+            return (
+              <CardFormInput
+                {...props}
+                key={`${props.label.replace(" ", "")}__${props.type}`}
+              />
+            );
+          })}
 
           <button
             className="btn text-center col-12 p-3 mt-5"
