d2aeb04d8313dfb871081f4b10d95bbe78f437f9
Diego Duarte <duartqx@gmail.com>
2024-02-11 22:01:25 -0300

    cache helper functions, createDriver and deleteDriver

--- src/presentation/react/src/services/driver/driver.js
+++ src/presentation/react/src/services/driver/driver.js
@@ -17,31 +17,50 @@ import httpClient from "../client";
  * }} Driver
  */
 
+/** @returns {string} */
+function getCacheKey() {
+  const authData = JSON.parse(localStorage.getItem("auth") || "{}");
+  return `cachedDriver__${authData?.token}`;
+}
+
+async function invalidateCache() {
+  localStorage.removeItem(getCacheKey())
+}
+
+/** @returns {Promise<Driver[] | null>} */
+async function getFromCache() {
+  const cachedDrivers = JSON.parse(localStorage.getItem(getCacheKey()) || "{}");
+
+  if (
+    cachedDrivers?.expiresAt &&
+    new Date(cachedDrivers?.expiresAt) > new Date()
+  ) {
+    return cachedDrivers.drivers;
+  }
+  return null;
+}
+
+/** @param {Driver[]} drivers */
+async function setToCache(drivers) {
+  const newCachedDrivers = {
+    drivers: drivers,
+    expiresAt: new Date().setMinutes(new Date().getMinutes() + 5),
+  };
+  localStorage.setItem(getCacheKey(), JSON.stringify(newCachedDrivers));
+}
+
 /** @returns {Promise<Driver[]>} */
 async function companyDrivers() {
   try {
-    const authData = JSON.parse(localStorage.getItem("auth") || "{}");
-    const cacheDriverKey = `cachedDriver__${authData?.token}`;
-
-    const cachedDriver = JSON.parse(
-      localStorage.getItem(cacheDriverKey) || "{}"
-    );
-
-    if (
-      cachedDriver?.expiresAt &&
-      (new Date(cachedDriver?.expiresAt) > new Date())
-    ) {
-      return cachedDriver.drivers;
+    const cachedDrivers = await getFromCache();
+    if (cachedDrivers?.length) {
+      return cachedDrivers;
     }
 
     const res = await httpClient().get("/driver");
 
     if (res.data) {
-      const newCachedDrivers = {
-        drivers: res.data,
-        expiresAt: new Date().setMinutes(new Date().getMinutes() + 5),
-      };
-      localStorage.setItem(cacheDriverKey, JSON.stringify(newCachedDrivers));
+      setToCache(res.data);
     }
 
     return res.data || [];
@@ -51,4 +70,40 @@ async function companyDrivers() {
   }
 }
 
-export { companyDrivers };
+/** @returns {Promise<Driver>} */
+async function createDriver({ name, email, license }) {
+  try {
+    const res = await httpClient().post("/driver", {
+      license_id: license,
+      user: {
+        name: name,
+        email: email,
+      },
+    });
+
+    if (res.data) {
+      invalidateCache();
+      return res.data;
+    }
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
+}
+
+/**
+ * @param {number} id
+ * @returns {Promise<boolean>}
+ */
+async function deleteDriver({id}) {
+  try {
+    await httpClient().delete(`/driver/${id}`)
+    invalidateCache()
+    return true
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
+
+export { companyDrivers, createDriver, deleteDriver };
