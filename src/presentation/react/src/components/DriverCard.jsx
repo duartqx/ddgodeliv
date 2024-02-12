3d502343057f1936b418228d210843ed1d6a06b7
Diego Duarte <duartqx@gmail.com>
2024-02-11 21:59:45 -0300

    DriverCard deleteHandler

--- src/presentation/react/src/components/DriverCard.jsx
+++ src/presentation/react/src/components/DriverCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Card from "./Card";
 
-/** @param {{ driver: import("../services/driver/driver").Driver }} props */
-export default function DriverCard({ driver }) {
+/** @param {{
+ * deleteHandler: Function
+ * driver: import("../services/driver/driver").Driver
+ * }} props */
+export default function DriverCard({ deleteHandler, driver }) {
   const parts = [
     { label: "", value: driver.user.email, border: true },
     { label: "", value: driver.license_id, border: false },
   ];
-  return <Card title={driver.user.name} parts={parts} />;
+  return (
+    <Card
+      title={driver.user.name}
+      parts={parts}
+      deleteHandler={deleteHandler}
+    />
+  );
 }
