d9bcc5388056bb43ff1551aaa5e2eba6356d829b
Diego Duarte <duartqx@gmail.com>
2024-02-11 22:00:59 -0300

    Error noPadding

--- src/presentation/react/src/components/Error.jsx
+++ src/presentation/react/src/components/Error.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 /** @param {{ err: string }} props */
-export default function Error({ err }) {
-  return <div className="my-3 py-2 alert alert-danger">{err}</div>;
+export default function Error({ err, noPadding }) {
+  return (
+    <div className={`${noPadding ? "" : "my-3 py-2"} alert alert-danger`}>
+      {err}
+    </div>
+  );
 }
